d7cc0f7c6572d54986e55b6c545d665e89816280
Andrei Labadaev <andrei-labadaev@mail.ru>
2024-12-20 11:52:39 +0300

    uppdate

---
+++ src/assets/js/app.js
@@ -0,0 +1,9 @@
+
+// vendor
+--include('vendor/inputmask.min.js')
+--include('vendor/htmlRootLock.js')
+--include('vendor/htmlRootUnlock.js')
+--include('vendor/modal.js') 
+
+// components
+--include('components/mask-phone.js')
